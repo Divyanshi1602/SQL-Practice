8a108716f9b650ccfbe6deb40233069e845a8db1
Divyanshi1602 <66871392+Divyanshi1602@users.noreply.github.com>
2020-09-30 12:48:46 +0530

    Add files via upload

---
+++ not_boring_movies.sql
@@ -0,0 +1,8 @@
+#X city opened a new cinema, many people would like to go to this cinema. The cinema also gives out a poster indicating the movies’ ratings and descriptions.
+#Please write a SQL query to output movies with an odd numbered ID and a description that is not 'boring'. Order the result by rating.
+
+select *
+from cinema
+where description<>'boring'
+and id%2<>0
+order by rating desc
